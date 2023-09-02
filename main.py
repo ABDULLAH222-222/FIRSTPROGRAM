ddffaddd8a6b36a054d9b4e88886dd209e9f7a39
ABDULLAH222-222 <143882858+ABDULLAH222-222@users.noreply.github.com>
2023-09-02 04:23:05 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+meme_dict = {
+            "КРИНЖ": "Что-то очень странное или стыдное",
+            "ЛОЛ": "Что-то очень смешное",
+            }
+word = input("Введите непонятное слово (большими буквами!): ")
+print(meme_dict.keys())
+            
+            
+            
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print('я не знаю такого слова ')
